b27c5914a6d517f84d4b3b77b7433a7a24501871
Graham Williams <graham.williams@togaware.com>
2016-10-02 15:11:29 +0800

    Add main and sub arguments. Include ggVarImp() for rpart.

--- R/ggVarImp.R
+++ R/ggVarImp.R
@@ -1,15 +1,33 @@
 ggVarImp <- function(model, ...) UseMethod("ggVarImp")
 
-ggVarImp.randomForest <- function(model)
+ggVarImp.randomForest <- function(model, 
+                                  main="Random Forest Variable Importance",
+                                  sub=genPlotTitleCmd(vector=TRUE),
+                                  ...)
 {
-  importance(model) %>%
+  randomForest::importance(model) %>%
     data.frame() %>%
     dplyr::mutate(Variable=row.names(.)) %>%
-    tidyr::gather(Measure, Value, -Variable) %>%
-    ggplot2::ggplot(ggplot2::aes(x=Variable, y=Value, fill=Variable)) +
+    tidyr::gather(Measure, Importance, -Variable) %>%
+    ggplot2::ggplot(ggplot2::aes(x=Variable, y=Importance, fill=Variable)) +
     ggplot2::geom_bar(stat="identity") +
     ggplot2::facet_wrap(~ Measure) +
-    ggplot2::theme(axis.title.x=ggplot2::element_blank(),
-                   axis.text.x=ggplot2::element_blank(),
-                   axis.ticks.x=ggplot2::element_blank())  
+    ggplot2::ggtitle(label=main) +
+    ggplot2::xlab(label=sub) +
+    ggplot2::theme(axis.text.x=ggplot2::element_text(angle=45, hjust=1),
+                   legend.position="none")
+  #  ggplot2::theme(axis.text.x=ggplot2::element_blank(),
+  #                 axis.ticks.x=ggplot2::element_blank())  
+}
+
+ggVarImp.rpart <- function(model, main, sub, ...)
+{
+  model$variable.importance %>%
+    data.frame() %>%
+    magrittr::set_names("Importance") %>%
+    dplyr::mutate(Variable=row.names(.)) %>%
+    ggplot2::ggplot(ggplot2::aes(x=Variable, y=Importance, fill=Variable)) +
+    ggplot2::geom_bar(stat="identity") +
+    ggplot2::theme(axis.text.x=ggplot2::element_text(angle=45, hjust=1),
+                   legend.position="none")
 }
