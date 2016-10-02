c95a06626ab47a804d3f0bc8e07820fb01379efe
Graham Williams <graham.williams@togaware.com>
2016-10-02 15:12:43 +0800

    Initial import.

---
+++ R/executeModelRxDForest.R
@@ -0,0 +1,67 @@
+#' Build a random forest based from xdf dataset.
+#' 
+#' Time-stamp: <2016-09-24 11:04:46 Graham Williams>
+#'
+executeModelRxDForest <- function()
+{
+  # Identify the model specific constants.
+  
+  TV   <- "rf_textview"
+  NAME <- commonName(crv$RXDFOREST)
+  PKG  <- "RevoScaleR"
+  FUNC <- "rxDForest"
+  VAR  <- "crs$rf"
+  TYPE <- Rtxt("Classification")
+  DESC <- Rtxt("build an xdf based random forest model")
+  
+  # Check package prerequisites.
+  
+  if (! packageIsAvailable(PKG, DESC)) return(FALSE)
+
+  # Construct the formula for the model build.
+
+  frml <- paste(crs$target, "~", paste(crs$input, collapse=" + "))
+
+  # Variables to be included --- a string of indicies.
+
+  # included <- getIncludedVariables()
+  included <- "c(crs$input, crs$target)" # 20110102
+
+  # Some convenience booleans
+
+  sampling   <- not.null(crs$train)
+  including  <- not.null(included)
+  subsetting <- sampling || including
+
+  # Commands.
+
+  build.cmd <- paste0(VAR, " <- ", PKG, "::", FUNC, "(", frml, ",\n",
+                      "                                 data=crs$xdf.split[[1]]",
+                      ")")
+
+  print.cmd <- paste0("print(", VAR, ")")
+
+  startLog(NAME)
+
+  # Build the model.
+
+  appendLog(sprintf(Rtxt("Build the %s model."), NAME), build.cmd)
+  start.time <- Sys.time()
+  result <- try(eval(parse(text=build.cmd)), silent=TRUE)
+  time.taken <- Sys.time() - start.time
+
+  # Show the results.
+
+  resetTextview(TV)
+  setTextview(TV,
+              sprintf(Rtxt("Summary of the %s model for %s (built using '%s'):"),
+                      NAME, TYPE, FUNC),
+              "\n\n",
+              collectOutput(print.cmd))
+
+  # Finish up.
+
+  reportTimeTaken(TV, time.taken, NAME)
+
+  return(TRUE)
+}
