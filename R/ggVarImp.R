a6bafd6c112769a5fafe4e5068eb96b785e05685
Graham Williams <graham.williams@togaware.com>
2016-11-12 20:03:58 +0530

    Rotate the VarImp plots and arrange descending.

--- R/ggVarImp.R
+++ R/ggVarImp.R
@@ -8,26 +8,37 @@ ggVarImp.randomForest <- function(model,
   randomForest::importance(model) %>%
     data.frame() %>%
     dplyr::mutate(Variable=row.names(.)) %>%
+    dplyr::arrange(desc(MeanDecreaseAccuracy)) %>%
     tidyr::gather(Measure, Importance, -Variable) %>%
+    dplyr::mutate(Variable=factor(Variable, levels=rev(unique(Variable)))) %>%
     ggplot2::ggplot(ggplot2::aes(x=Variable, y=Importance, fill=Variable)) +
-    ggplot2::geom_bar(stat="identity") +
+    ggplot2::geom_bar(stat="identity", position="identity") +
     ggplot2::facet_wrap(~ Measure) +
     ggplot2::ggtitle(label=main) +
-    ggplot2::xlab(label=sub) +
+    ggplot2::ylab(label=sub) +
+    ggplot2::xlab("") +
+    ggplot2::coord_flip() +
     ggplot2::theme(axis.text.x=ggplot2::element_text(angle=45, hjust=1),
                    legend.position="none")
-  #  ggplot2::theme(axis.text.x=ggplot2::element_blank(),
-  #                 axis.ticks.x=ggplot2::element_blank())  
 }
 
-ggVarImp.rpart <- function(model, main, sub, ...)
+ggVarImp.rpart <- function(model,
+                           main="Decision Tree Variable Importance",
+                           sub=genPlotTitleCmd(vector=TRUE),
+                           ...)
 {
   model$variable.importance %>%
     data.frame() %>%
     magrittr::set_names("Importance") %>%
     dplyr::mutate(Variable=row.names(.)) %>%
+    dplyr::arrange(desc(Importance)) %>%
+    dplyr::mutate(Variable=factor(Variable, levels=rev(unique(Variable)))) %>%
     ggplot2::ggplot(ggplot2::aes(x=Variable, y=Importance, fill=Variable)) +
-    ggplot2::geom_bar(stat="identity") +
+    ggplot2::geom_bar(stat="identity", position="identity") +
+    ggplot2::ggtitle(label=main) +
+    ggplot2::ylab(label=sub) +
+    ggplot2::xlab("") +
+    ggplot2::coord_flip() +
     ggplot2::theme(axis.text.x=ggplot2::element_text(angle=45, hjust=1),
                    legend.position="none")
 }
@@ -40,10 +51,14 @@ ggVarImp.rxDForest <- function(model,
   model$importance %>%
     data.frame() %>%
     dplyr::mutate(Variable=row.names(.)) %>%
+    dplyr::arrange(desc(IncNodePurity)) %>%
+    dplyr::mutate(Variable=factor(Variable, levels=rev(unique(Variable)))) %>%
     ggplot2::ggplot(ggplot2::aes(x=Variable, y=IncNodePurity, fill=Variable)) +
-    ggplot2::geom_bar(stat="identity") +
+    ggplot2::geom_bar(stat="identity", position="identity") +
     ggplot2::ggtitle(label=main) +
-    ggplot2::xlab(label=sub) +
+    ggplot2::ylab(label=sub) +
+    ggplot2::xlab("") +
+    ggplot2::coord_flip() +
     ggplot2::theme(axis.text.x=ggplot2::element_text(angle=45, hjust=1),
                    legend.position="none")
 }
